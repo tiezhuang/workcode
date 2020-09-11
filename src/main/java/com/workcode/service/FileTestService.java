c15a92fd39e2e9bb37bfcbd1f8df5838f6a49037
tiezhuang <1054974157@qq.com>
2020-09-11 11:15:07 +0800

    哈哈

---
+++ src/main/java/com/workcode/service/FileTestService.java
@@ -0,0 +1,25 @@
+package com.workcode.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.workcode.entity.FileTest;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.workcode.entity.User;
+
+import java.util.Map;
+
+/**
+ * <p>
+ *  服务类
+ * </p>66666
+ *
+ * @author 铁壮
+ * @since 2020-08-31
+ */
+
+public interface FileTestService extends IService<FileTest> {
+    /**
+     *
+     * @param fileTestPagePage
+     */
+    void getFileByPage(Page<FileTest> fileTestPagePage, Map<String, Object> fileParams);
+}
